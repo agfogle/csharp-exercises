ba3e257a968afb796ab9aaea2e7f6ba76f29d4f0
Ashley Fogle <ms.ashleygreene@gmail.com>
2019-10-22 20:33:39 -0400

    Completed application to calculate area of circle based on input from user. Includes validation.

---
+++ Area/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            Console.WriteLine("Enter a radius");
+            radius = double.Parse(Console.ReadLine());
+            double pi = 3.14;
+            
+
+            while (radius<0)
+                    {
+                Console.WriteLine("Input is invalid");
+                Console.WriteLine("Enter a radius");
+                radius = double.Parse(Console.ReadLine());
+            }
+
+            double Area = pi * radius * radius;
+            Console.WriteLine($"The area of a circle with {radius} is: {Area}");
+            
+            
+        }
+    }
+}
